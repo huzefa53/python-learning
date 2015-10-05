d83e484146d7202084eff2946513ed9c7d61c93e
huzefa <huzefa.hamdard53@gmail.com>
2015-10-04 23:02:46 -0700

    list.py

---
+++ python-learning/list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+list = [ 'huzefa', 'hamdard', 786, '22.43' ]
+tinylist = [122, 'huzefa' ]
+
+print list         #print complete list
+print list[0]      #print first element of list
+print list[1:3]   # print from first to third element of list
+print list [2:]   #print elements starting from 3rd element
+print tinylist * 2  #print list 2 times
+print list + tinylist  #print concateneted list
